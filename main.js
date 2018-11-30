bf296494f6f6aedc30bbc502fe6d9ca0dda83553
3xoses <sebastian.staniak@interia.pl>
2018-11-30 20:41:12 +0100

    add on scroll animation for .card

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 $('.carousel').carousel()
+
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.card'
+})
+.setClassToggle('.card', 'show')
+.addTo(controller);
