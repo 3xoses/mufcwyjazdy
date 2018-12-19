308d1dcd89b210dee000fcbf0bcc45d0360d2de8
3xoses <sebastian.staniak@interia.pl>
2018-12-19 21:09:34 +0100

    change footer, add info to match card, add up circle

--- main.js
+++ main.js
@@ -8,10 +8,4 @@ var scene = new ScrollMagic.Scene({
 .setClassToggle('.card', 'show')
 .addTo(controller);
 
-var controller = new ScrollMagic.Controller();
-
-var scene = new ScrollMagic.Scene({
-    triggerElement: '.col-sm'
-})
-.setClassToggle('.col-sm', 'show')
-.addTo(controller);
+CookieAlert.init();
