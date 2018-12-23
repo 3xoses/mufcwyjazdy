4b86918b83239e303f9d5425aa2e086532675f33
3xoses <sebastian.staniak@interia.pl>
2018-12-23 18:11:06 +0100

    fix bug in circle and article

--- main.js
+++ main.js
@@ -10,12 +10,3 @@ var scene = new ScrollMagic.Scene({
 
 CookieAlert.init();
 
-function myFunction() {
-    var x = document.getElementById("read-more");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
